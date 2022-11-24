8b7969158fe8cd1d4841cef4c60188750a6e6da1
afzal75 <afzalbhuiyan16@gmail.com>
2022-11-24 23:10:05 +0600

    category api craeted

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 
-
 const app = express();
-const port = process.env.PORT || 5000;
 
-// middle wares
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f7wnwq6.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f7wnwq6.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+
+async function run(){
+    try{
+        const phoneCategoryCollection = client.db('phoneService').collection('phoneCategory');
+
+        app.get('/phoneCategory', async(req, res) => {
+            const query = {};
+            const categories = await phoneCategoryCollection.find(query).toArray();
+            res.send(categories)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log)
 
 
 
 app.get('/', (req, res) => {
-    res.send('assingnment 12 server is running');
-});
-
-app.listen(port, () => {
-    console.log(`Assignment 12 server running on ${port}`);
+    res.send('assignement 12 server is running')
 })
+
+app.listen(port, () => console.log(`Assignment 12 running on ${port}`))
