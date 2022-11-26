49ffad8550cca7285bf281bcac5e083714a8fded
afzal75 <afzalbhuiyan16@gmail.com>
2022-11-26 23:34:34 +0600

    user put database api

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ async function run() {
     try {
         const phoneCategoryCollection = client.db('phoneService').collection('phoneCategory');
         const productsCollection = client.db('phoneService').collection('products');
-        const bookingsCollection = client.db('phoneService').collection('bookings')
+        const bookingsCollection = client.db('phoneService').collection('bookings');
+        const usersCollection = client.db('phoneService').collection('users');
 
         // all category api
         app.get('/phoneCategory', async (req, res) => {
@@ -48,6 +49,36 @@ async function run() {
             console.log(booking)
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
+        });
+
+        
+
+        // user put to db
+
+        app.put('/user/:email', async(req, res) => {
+            const email = req.params.email;
+            console.log(email);
+            const filter = {email: email};
+            const options = {upsert: true};
+            const updateDoc = {
+                $set: {
+                    role: 'buyer'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send({
+                status: "success",
+                message: "user added to db",
+                data: result
+            })
+        })
+
+        // user api created
+
+        app.get('/users/:email', async(req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne(email)
+            res.send(user)
         })
 
     }
